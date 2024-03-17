791fcfbe36e2f163811a8a38378b52abaffd6148
VladTBK <most_loginVlad.TBK@proton.me>
2024-03-17 23:03:16 +0200

    hi mom

--- Easy/climbing-staris.py
+++ Easy/climbing-staris.py
@@ -1,19 +1,18 @@
 # # recursive approach, but not approved by LeetCode because of extended time
 # class Solution:
-#     count = 0
-
 #     def climbStairs(self, n: int) -> int:
 #         if n == 0:
 #             return 1
 #         if n <= -1:
 #             return 0
 
-#         self.count = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-#         return self.count
+#         return self.climbStairs(n - 1) + self.climbStairs(n - 2)
 
 
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 1
         dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 1
@@ -25,5 +24,4 @@ class Solution:
 
 
 sol = Solution()
-
-print(sol.climbStairs(45))
+print(sol.climbStairs(0))
